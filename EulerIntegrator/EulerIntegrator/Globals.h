1bd41720841afd4ae350d1e3308735fc2cc88623
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2019-11-09 16:31:52 +0100

    Added Some Useful Typedefs

--- EulerIntegrator/EulerIntegrator/Globals.h
+++ EulerIntegrator/EulerIntegrator/Globals.h
@@ -4,6 +4,15 @@
 
 
 using namespace std;
+typedef signed char	int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef float float32;
+typedef double float64;
+
 
 
 
