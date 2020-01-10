635292870b86989b8d4b547bfb7da2dcd43bdc85
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2020-01-10 19:04:59 +0100

    Collision of the projectile now beheaves correctly

--- Aimbot/EulerIntegrator/ObjectStruct.h
+++ Aimbot/EulerIntegrator/ObjectStruct.h
@@ -9,9 +9,8 @@
 //each object has a name, position, velocity and mass. It also has a force, which is set to 0 at the start of every loop
 struct Object {
 
-public:
+private:
 	dPoint force; //In newtons WARNING, DO NOT ACCES THE VARIABLE WITHOUT EXPLICIT PERMISSION FROM THE PHYSICS GUYS, IF YOU NEED TO ADD A FORCE USE THE FUNCTION .AddForce()
-					// Why dont you put it in fucking private you fck morons? Att: Jose :D
 	p2SString name;
 	dPoint pos; //In meters
 	dPoint speed;  //In meters/second
